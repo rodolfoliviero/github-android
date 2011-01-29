5ee7ddea8f324cfed77b7d8cbf066e07a5439d4c
rodolfoliviero <rodolfoliviero@gmail.com>
2011-01-29 11:41:53 -0200

    first commit

---
+++ src/com/github/GithubActivity.java
@@ -0,0 +1,15 @@
+package com.github;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GithubActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
